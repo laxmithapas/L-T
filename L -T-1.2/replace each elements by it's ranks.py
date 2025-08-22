8ecf9309d80d59c47a004243b81a30965500bf2c
laxmithapas <shakharthapa2004@gmail.com>
2025-08-22 14:01:20 +0530

    added files

--- L -T-1.2/replace each elements by it's ranks.py
+++ L -T-1.2/replace each elements by it's ranks.py
@@ -1,4 +1,14 @@
-n = list(map(int, input("Enter numbers separated by space: ").split()))
-ranks = {val: rank for rank, val in enumerate(sorted(set(n)), start=1)}
-replaced = [ranks[val] for val in n]
-print("Replaced array is:", replaced)
+def replace_with_rank(arr):
+    
+    sorted_unique = sorted(set(arr))
+
+    
+    rank_map = {value: rank+1 for rank, value in enumerate(sorted_unique)}
+
+    
+    return [rank_map[x] for x in arr]
+
+
+arr = [40, 10, 20, 40, 30]
+print("Original:", arr)
+print("Ranked:", replace_with_rank(arr))
