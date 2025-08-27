b8fa8a50437dc2a02b6bc3d0b022bd22d58adf8e
laxmithapas <shakharthapa2004@gmail.com>
2025-08-27 14:31:47 +0530

    added files

--- L - T - 1.4/linkedlist.py
+++ L - T - 1.4/linkedlist.py
@@ -10,11 +10,28 @@ class LinkedList:
     
     def insertbegg(self, d):
         new_node = Node(d)
-        if self.head is None:
+        if self.head == None:
             self.head = new_node
         else:
             new_node.next = self.head
             self.head = new_node
+    def insertend(self, d):
+        new_node = Node(d)
+        if self.head == None:
+            self.head = new_node
+        else:
+            Last = self.head
+            while Last.next != None:
+                Last = Last.next
+            Last.next = new_node
+    def insertatany(self, d, position):
+        new_node = Node(d)
+        if self.head == None:
+            self.head = new_node
+        else:
+            temp = self.head
+            for i in range(position - 1):
+                temp = temp.next
 
     def printlist(self):
         temp = self.head
