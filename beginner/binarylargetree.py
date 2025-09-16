92d822152289f8a91346211ed67d7882314399cd
laxmithapas <shakharthapa2004@gmail.com>
2025-09-16 14:01:51 +0530

    added

---
+++ beginner/binarylargetree.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def insert(root, value):
+    if root is None:
+        return Node(value)
+    if value < root.value:
+        root.left = insert(root.left, value)
+    else:
+        root.right = insert(root.right, value)
+    return root
+    
